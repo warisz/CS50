a4a61c2433ccf0f3483ac4005efdbe4752b0b0e3
Waris Z <wariszahoor0@gmail.com>
2019-01-28 16:18:03 -0500

    mario.c

---
+++ mario.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    do{
+        height = get_int("Height: ");
+    }
+    while(height<1 || height>8);
+
+    for(int i=1; i<=height; i++){
+        for(int k=0; k<height-i; k++){
+            printf(" ");
+        }
+
+        for(int j=0; j<i; j++){
+            printf("#");
+        }
+
+        printf(" ");
+
+        for(int j=0; j<i; j++){
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}
