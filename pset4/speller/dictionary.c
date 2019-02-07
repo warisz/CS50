58bd2f2a7f2a6d0c044e8d96a9b8c28d9f7da404
Waris Z <wariszahoor0@gmail.com>
2019-02-06 23:56:59 -0500

    pset4

---
+++ pset4/speller/dictionary.c
@@ -0,0 +1,107 @@
+// Implements a dictionary's functionality
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "dictionary.h"
+
+// Represents number of buckets in a hash table
+#define N 26
+
+// Represents a node in a hash table
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+}
+node;
+
+// Represents a hash table
+node *hashtable[N];
+
+// Hashes word to a number between 0 and 25, inclusive, based on its first letter
+unsigned int hash(const char *word)
+{
+    return tolower(word[0]) - 'a';
+}
+
+// Loads dictionary into memory, returning true if successful else false
+bool load(const char *dictionary)
+{
+    // Initialize hash table
+    for (int i = 0; i < N; i++)
+    {
+        hashtable[i] = NULL;
+    }
+
+    // Open dictionary
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        unload();
+        return false;
+    }
+
+    // Buffer for a word
+    char word[LENGTH + 1];
+
+
+    // Insert words into hash table
+    while (fscanf(file, "%s", word) != EOF)
+    {
+        char letter = hash(word);
+        int i = atoi(&letter);
+        node *newVal = malloc(sizeof(node));
+
+        if (!newVal){
+            return 1;
+        }
+
+
+        for(int k=0; k<LENGTH; k++){
+            if(word[k] == '\n'){
+                newVal->word[k] = '\0';
+                break;
+            }
+            newVal->word[k] = word[k];
+        }
+        newVal->next = NULL;
+
+        if(hashtable[i] == NULL){
+            hashtable[i] = newVal;
+        }else{
+            newVal->next = hashtable[i]->next;
+            hashtable[i] = newVal;
+        }
+
+    }
+
+    // Close dictionary
+    fclose(file);
+
+    // Indicate success
+    return true;
+}
+
+// Returns number of words in dictionary if loaded else 0 if not yet loaded
+unsigned int size(void)
+{
+    // TODO
+    return 0;
+}
+
+// Returns true if word is in dictionary else false
+bool check(const char *word)
+{
+    // TODO
+    return false;
+}
+
+// Unloads dictionary from memory, returning true if successful else false
+bool unload(void)
+{
+    // TODO
+    return false;
+}
